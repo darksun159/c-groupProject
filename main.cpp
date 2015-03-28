7ceb0c6e719c39093392c47d3aa5578f9f3b8705
Bartek Banachewicz <bananu7@o2.pl>
2015-03-29 00:37:50 +0100

    Added args catching and the file for VCS class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
-int main() {
-    std::cout << "Hello noobles" << '\n';
+#include "LoungeVCS.hpp"
+
+int main(int argc, char* argv[]) {
+    std::vector<std::string> args (argv, argv + argc);
+
+    std::cout << "Lounge VCS v0.0.0.0" << '\n';
+
+    for (auto const& arg : args) {
+        std::cout << arg << '\n';
+    }
 }
