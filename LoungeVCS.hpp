2bcd3390cc0f25ebbc969686cc84fbf3748846fa
Bartek Banachewicz <bananu7@o2.pl>
2015-03-29 00:52:28 +0100

    Drafted VCS api

--- LoungeVCS.hpp
+++ LoungeVCS.hpp
@@ -1,1 +1,36 @@
 #pragma once
+#include <set>
+#include <map>
+
+namespace Lounge {
+
+    static const char* vcsDir = ".lounge";
+
+    using Path = std::string;
+    using RevisionNumber = int;
+
+    using RepoRevision = std::map<Path, RevisionNumber>;
+    using Index = std::map<RevisionNumber, RepoRevision>;
+
+    class Vcs {
+    private:
+        Index index;
+
+    public:
+        // TODO
+        /// Initializes the vcs dir 
+        void init();
+
+        // TODO
+        /// Copies the appropriate file revision to the working dir
+        void checkoutFile(Path path, RevisionNumber revision) const;
+
+        // TODO
+        /// Checkouts the whole working copy
+        void checkout(RevisionNumber revision);
+
+        // TODO
+        /// Copies the appropriate file to the vcs dir with an incremented number
+        void commit(Path path, RevisionNumber revision);
+    };
+}
