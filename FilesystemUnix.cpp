babd7567200bfb624c541b29d46ec72247b8e507
Nooble <Noogle@Gmail.com>
2015-03-28 18:57:10 -0700

    Implemented fileCopy() to filesystem library

--- FilesystemUnix.cpp
+++ FilesystemUnix.cpp
@@ -1,11 +1,19 @@
 #include "Filesystem.hpp"
 
 #include <sys/stat.h>
+#include <fstream>
 
 namespace Lounge {
     namespace Filesystem {
         void createDirectory(std::string path) {
             mkdir(path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+        }
+
+        void fileCopy(std::string source, std::string destination) {
+        	std::ifstream src(source, std::ios::binary);
+        	std::ofstream dst(destination, std::ios::binary);
+
+        	dst << src.rdbuf();
         }
     }
 }
--- FilesystemWindows.cpp
+++ FilesystemWindows.cpp
@@ -1,11 +1,19 @@
 #include "Filesystem.hpp"
 
 #include <Windows.h>
+#include <fstream>
 
 namespace Lounge {
     namespace Filesystem {
         void createDirectory(std::string path) {
             CreateDirectory(path.c_str(), NULL);
+        }
+
+        void fileCopy(std::string source, std::string destination) {
+        	std::ifstream src(source, std::ios::binary);
+        	std::ofstream dst(destination, std::ios::binary);
+
+        	dst << src.rdbuf();
         }
     }
 }
