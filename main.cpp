4e7af67493a183c954ffb54228be50fe82209996
Cody <darksun.darker@gmail.com>
2015-03-28 19:22:08 -0500

    added help message

--- main.cpp
+++ main.cpp
@@ -4,12 +4,21 @@
 
 #include "LoungeVCS.hpp"
 
+void argparse(vecter<std::string>);
+
 int main(int argc, char* argv[]) {
     std::vector<std::string> args (argv, argv + argc);
+	argparse(args);
 
     std::cout << "Lounge VCS v0.0.0.0" << '\n';
 
     for (auto const& arg : args) {
         std::cout << arg << '\n';
     }
+}
+
+void argparse(vecter<std::string> args){
+	for(int x = 0; x <= args.size(); x++){
+		if(args[x] == "-h" || args[x] == "--help") std::cout<<"This is a help message...\n";
+	}
 }
